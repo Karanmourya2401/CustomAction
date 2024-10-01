f4179ef7957c82cd2249f4ec3af56614d3e9894c
karan <karanmourya0590@gmail.com>
2024-10-01 10:47:52 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 
-console.log('hello');
+// console.log('hello');
+
+
+const core = require('@actions/core');
+ 
+try {
+    const person = process.env.INPUT_PERSON;  // Access the input
+    console.log(`Hello ${person} from custom js actions`);  // Use backticks for string interpolation
+} catch (error) {
+    core.setFailed(`Action failed: ${error}`);
+}
+ 
